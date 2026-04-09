76520a418e541b560d7020f83638621103cfe277
Logan Bolton <loganbolton101@gmail.com>
2026-04-09 00:02:23 -0500

    better code!

--- diffusion.py
+++ diffusion.py
@@ -1,34 +1,25 @@
 import numpy as np
 import torch
 
-
 class DiffusionConstants:
-    def __init__(self, timesteps, x0):
+    def __init__(self, timesteps):
         self.timesteps = timesteps
-        self.x0 = x0
-        
-        betas = self.get_betas(timesteps)
-        alphas = self.get_alpha(betas, timesteps)
-        alpha_bar = torch.cumprod(alphas, dim=0)
+        self.betas = self.get_betas(timesteps)
+        self.alphas = 1 - self.betas
+
+        alpha_bar = torch.cumprod(self.alphas, dim=0)
         self.sqrt_alpha_bar = torch.sqrt(alpha_bar)
         self.sqrt_one_minus_alpha_bar = torch.sqrt(1-alpha_bar)
-        self.epsilon = torch.randn_like(x0)
 
-    def get_betas(timesteps):
+    def get_betas(self, timesteps):
         beta_start = 0.0001
         beta_end = 0.02
-        betas = np.linspace(beta_start, beta_end, timesteps)
+        betas = torch.linspace(beta_start, beta_end, timesteps)
         return betas
-        
-    def get_alphas(betas, timesteps):
-        alphas = np.empty(timesteps)
-        for t in range(timesteps):
-            alphas[t] = 1 - betas[t]
-        return alphas
-
     
-    def add_noise(t):
-        reduced_x = self.sqrt_alpha_bar[t] * self.x0
-        noise = self.sqrt_one_minus_alpha_bar[t] * self.epsilon
+    def add_noise(self, t, x0):
+        epsilon = torch.randn_like(x0)
+        reduced_x = self.sqrt_alpha_bar[t] * x0
+        noise = self.sqrt_one_minus_alpha_bar[t] * epsilon
         x_t = reduced_x + noise
-        return x_t
+        return x_t, epsilon
