4932949f731091d584469564d12e9b34356f11c2
Log <loganbolton101@gmail.com>
2026-05-26 23:22:39 -0500

    add basic ptaches

--- dit.py
+++ dit.py
@@ -35,3 +35,30 @@ class DiTBlock(nn.Module):
         x = x + h * a2.unsqueeze(1)
 
         return x
+    
+class DiT(nn.Module):
+    def __init__(self, img_size=64, patch_size=4, hidden_dim=512, num_heads=8, num_layers=12):
+        super().__init__()
+        self.patch_proj = nn.Conv2d(
+            in_channels=3,
+            out_channels=hidden_dim,
+            kernel_size=patch_size,
+            stride_size=patch_size
+        )
+        self.num_patches = (img_size // patch_size) ** 2
+        self.pos_embed = nn.Parameter(torch.zeroes(1, self.num_patches, hidden_dim))
+        
+    
+    def patchify(self, x):
+        x = self.patch_proj(x) # b, hidden_dim, 16, 16
+        x = x.flatten(2) # b, hidden_dim, 256
+        x = x.transpose(1, 2) # b, 256, hidden_dim
+        return x
+    
+    def unpatchify(self, x):
+        pass
+    
+    def forward(self, x, t, text_embed):
+        pass
+    
+    
